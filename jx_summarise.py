93fcec60c6f2a5fd46752b564847cb86cb964a02
ninix <jennifertse1999@gmail.com>
2022-02-07 11:06:21 +0800

    summarise file testing 1

--- jx_summarise.py
+++ jx_summarise.py
@@ -1,20 +1,24 @@
 import torch
 import pandas as pd
+
 import matplotlib.pyplot as plt
+
 
 taskids = 36
 
 df = pd.DataFrame(columns=["sigma_pert", "l2_penalty", "RBM_dim", "GoF_reject_rate", "id_reject_rate"])
 
-baseline_df = pd.DataFrame(columns=["sigma_pert", "l2_penalty", "RBM_dim"])
+#baseline_df = pd.DataFrame(columns=["sigma_pert", "l2_penalty"])
 
 for taskid in range(taskids):
     try:
         result = torch.load('taskid{}/result.pt'.format(taskid), map_location=torch.device('cpu'))
         print(result)
         df.loc[len(df.index)] = list(result.values())
-        baseline_df.loc[len(baseline_df.index)] = [result['sigma_pert'], result['l2_penalty'], result['RBM_dim']]
+        #baseline_df.loc[len(baseline_df.index)] = [result['sigma_pert'], result['l2_penalty']]
     except:
         print('missing')
 
 # TODO: finish this part, construct a table
+print(df)
+
