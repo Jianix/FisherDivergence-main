ee20364afe19580c2b53d1eab733e5b2b35d21c3
ninix <jennifertse1999@gmail.com>
2022-02-07 15:42:29 +0800

    n_rej_iter = 50 test

--- jx_summarise.py
+++ jx_summarise.py
@@ -3,23 +3,22 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-
 taskids = 36
 
 df = pd.DataFrame(columns=["sigma_pert", "l2_penalty", "RBM_dim", "GoF_reject_rate", "id_reject_rate"])
 
-#baseline_df = pd.DataFrame(columns=["sigma_pert", "l2_penalty"])
+# baseline_df = pd.DataFrame(columns=["sigma_pert", "l2_penalty"])
 
 for taskid in range(taskids):
     try:
         result = torch.load('taskid{}/result.pt'.format(taskid), map_location=torch.device('cpu'))
-        print(result)
-        print(result.values())
         df.loc[len(df.index)] = list(result.values())
-        #baseline_df.loc[len(baseline_df.index)] = [result['sigma_pert'], result['l2_penalty']]
+
+        # baseline_df.loc[len(baseline_df.index)] = [result['sigma_pert'], result['l2_penalty']]
     except:
         print('missing')
 
 # TODO: finish this part, construct a table
-print(df)
+df_sorted = df.sort_values('RBM_dim')
 
+print(df_sorted)
